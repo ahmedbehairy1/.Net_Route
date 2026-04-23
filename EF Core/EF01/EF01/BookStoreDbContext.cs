9d35719f58351246c5f98fcce8f2a94d9f5416e0
ahmedbehairy1 <120663270+ahmedbehairy1@users.noreply.github.com>
2026-04-23 15:36:01 +0200

    firstcommit

---
+++ EF Core/EF01/EF01/BookStoreDbContext.cs
@@ -0,0 +1,21 @@
+﻿using EF01.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF01
+{
+    public class BookStoreDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=BookStoreDB;Trusted_Connection=True;TrustServerCertificate = true");
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author>Authors { get; set; }
+        public DbSet<Category>Categories { get; set; }
+    }
+}
---
+++ EF Core/EF01/EF01/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF01.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public decimal Price { get; set; }
+        public int NumberOfPages { get; set; }
+        public DateOnly Year { get; set; }
+        public bool InStock { get; set; }
+    }
+}
